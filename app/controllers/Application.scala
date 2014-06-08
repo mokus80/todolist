521609532f1609a7b72aec40592e418358df5b29
Nicole <mokus80@gmail.com>
2014-06-08 15:18:34 +0200

    nikki fixes all

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,28 @@
 package controllers
 
-import play.api._
+import models.Task
 import play.api.mvc._
+
+import play.api.data._
+import play.api.data.Form
 
 object Application extends Controller {
 
   def index = Action {
-    Ok("Hello World!")
+    Redirect(routes.Application.tasks)
   }
+
+  def tasks = Action {
+
+    val taskForm = Form(
+      "label" -> Forms.nonEmptyText
+    )
+
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
 
 }
